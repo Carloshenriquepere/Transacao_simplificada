a2d5ab5b600b9fab459679a205b235fb62ab37ec
Carlos Soares <carloshenriquepereirasoaress@gmail.com>
2023-09-25 15:31:26 -0300

    feat: create UserRepository

---
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserBydocument(String document);
+    Optional<User> findUserById(Long id);
+}
